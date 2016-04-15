e14b5084969398fb3aa6d3388a473144ae57f5e5
Vitalii Slobodianyk <vitalii.slobodianyk@oicr.on.ca>
2016-04-15 12:04:46 -0400

    Moved submitJob logic from DownloadResource to Client

--- dcc-download-client/src/main/java/org/icgc/dcc/download/client/DownloadClient.java
+++ dcc-download-client/src/main/java/org/icgc/dcc/download/client/DownloadClient.java
@@ -1,4 +1,25 @@
+/*
+ * Copyright (c) 2016 The Ontario Institute for Cancer Research. All rights reserved.                             
+ *                                                                                                               
+ * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
+ * You should have received a copy of the GNU General Public License along with                                  
+ * this program. If not, see <http://www.gnu.org/licenses/>.                                                     
+ *                                                                                                               
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY                           
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES                          
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT                           
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                                
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED                          
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;                               
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER                              
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN                         
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.icgc.dcc.download.client;
+
+import static org.icgc.dcc.common.core.util.stream.Collectors.toImmutableSet;
+import static org.icgc.dcc.download.core.model.DownloadDataType.CLINICAL;
+import static org.icgc.dcc.download.core.model.DownloadDataType.DONOR;
 
 import java.io.OutputStream;
 import java.util.List;
@@ -16,6 +37,8 @@ import org.icgc.dcc.download.core.model.DownloadDataType;
 import org.icgc.dcc.download.core.model.JobInfo;
 import org.icgc.dcc.download.core.model.JobProgress;
 import org.icgc.dcc.download.core.request.SubmitJobRequest;
+
+import com.google.common.collect.ImmutableSet;
 
 @RequiredArgsConstructor
 public class DownloadClient {
@@ -39,25 +62,12 @@ public class DownloadClient {
   public String submitJob(
       @NonNull Set<String> donorIds,
       @NonNull Set<DownloadDataType> dataTypes,
-      @NonNull JobInfo jobInfo) {
-    val submitJobRequest = SubmitJobRequest.builder()
-        .donorIds(donorIds)
-        .dataTypes(dataTypes)
-        .jobInfo(jobInfo)
-        .userEmailAddress(defaultEmail)
-        .build();
-
-    return httpClient.submitJob(submitJobRequest);
-  }
-
-  public String submitJob(
-      @NonNull Set<String> donorIds,
-      @NonNull Set<DownloadDataType> dataTypes,
       @NonNull JobInfo jobInfo,
       @NonNull String userEmailAddress) {
+    val submitDataTypes = resolveDataTypes(donorIds, dataTypes);
     val submitJobRequest = SubmitJobRequest.builder()
         .donorIds(donorIds)
-        .dataTypes(dataTypes)
+        .dataTypes(submitDataTypes)
         .jobInfo(jobInfo)
         .userEmailAddress(userEmailAddress)
         .build();
@@ -109,6 +119,24 @@ public class DownloadClient {
     val managedOut = out;
 
     return outputStream.streamArchiveInTarGz(managedOut, downloadId, downloadDataTypes);
+  }
+
+  private Set<DownloadDataType> resolveDataTypes(Set<String> donorIds, Set<DownloadDataType> dataTypes) {
+    val submitDataTypes = resolveSubmitDataTypes(dataTypes);
+    val sizes = getSizes(donorIds);
+
+    return submitDataTypes.stream()
+        .filter(dt -> sizes.get(dt) > 0L)
+        .collect(toImmutableSet());
+  }
+
+  private static Set<DownloadDataType> resolveSubmitDataTypes(Set<DownloadDataType> dataTypes) {
+    return dataTypes.contains(DONOR) ?
+        ImmutableSet.<DownloadDataType> builder()
+            .addAll(dataTypes)
+            .addAll(CLINICAL)
+            .build() :
+        dataTypes;
   }
 
 }
---
+++ dcc-download-client/src/test/java/org/icgc/dcc/download/client/DownloadClientTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2016 The Ontario Institute for Cancer Research. All rights reserved.                             
+ *                                                                                                               
+ * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
+ * You should have received a copy of the GNU General Public License along with                                  
+ * this program. If not, see <http://www.gnu.org/licenses/>.                                                     
+ *                                                                                                               
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY                           
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES                          
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT                           
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                                
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED                          
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;                               
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER                              
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN                         
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.icgc.dcc.download.client;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.icgc.dcc.download.core.model.DownloadDataType.DONOR;
+import static org.icgc.dcc.download.core.model.DownloadDataType.DONOR_EXPOSURE;
+import static org.icgc.dcc.download.core.model.DownloadDataType.DONOR_FAMILY;
+import static org.icgc.dcc.download.core.model.DownloadDataType.DONOR_THERAPY;
+import static org.icgc.dcc.download.core.model.DownloadDataType.SAMPLE;
+import static org.icgc.dcc.download.core.model.DownloadDataType.SPECIMEN;
+import static org.icgc.dcc.download.core.model.DownloadDataType.SSM_CONTROLLED;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import lombok.val;
+
+import org.icgc.dcc.download.client.io.ArchiveOutputStream;
+import org.icgc.dcc.download.core.model.DownloadDataType;
+import org.icgc.dcc.download.core.model.JobInfo;
+import org.icgc.dcc.download.core.request.SubmitJobRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DownloadClientTest {
+
+  @Mock
+  ArchiveOutputStream outputStream;
+  @Mock
+  HttpDownloadClient httpClient;
+
+  DownloadClient downloadClient;
+
+  @Before
+  public void setUp() {
+    downloadClient = new DownloadClient(outputStream, httpClient);
+  }
+
+  @Test
+  public void testSubmitClinicalJob() throws Exception {
+    // Setup
+    when(httpClient.getSizes(any(SubmitJobRequest.class)))
+        .thenReturn(ImmutableMap.<DownloadDataType, Long> builder()
+            .put(DONOR, 1L)
+            .put(SPECIMEN, 1L)
+            .put(SAMPLE, 0L)
+            .put(DONOR_EXPOSURE, 1L)
+            .put(DONOR_FAMILY, 0L)
+            .put(DONOR_THERAPY, 0L)
+            .put(SSM_CONTROLLED, 1L)
+            .build());
+
+    // Run
+    downloadClient.submitJob(
+        ImmutableSet.of("DO1"),
+        ImmutableSet.of(DONOR, SSM_CONTROLLED),
+        JobInfo.builder().build(),
+        "test@example.com");
+
+    // Verify
+    val argument = ArgumentCaptor.forClass(SubmitJobRequest.class);
+    verify(httpClient).submitJob(argument.capture());
+
+    val submitRequest = argument.getValue();
+    assertThat(submitRequest.getDataTypes()).containsOnly(DONOR, SSM_CONTROLLED, SPECIMEN, DONOR_EXPOSURE);
+  }
+
+}
